1c7867721be15a04f3a3c913d499fcbaec201318
Ferius057 <tayparov04@mail.ru>
2021-09-02 23:01:15 +0600

    fix: The Resync command was fixed

--- src/main/java/kz/ferius_057/ruminebot/command/Resync.java
+++ src/main/java/kz/ferius_057/ruminebot/command/Resync.java
@@ -10,10 +10,12 @@ import com.vk.api.sdk.objects.messages.responses.GetConversationMembersResponse;
 import com.vk.api.sdk.objects.users.Fields;
 import kz.ferius_057.ruminebot.VkApi;
 import kz.ferius_057.ruminebot.command.api.AbstractCommand;
+import kz.ferius_057.ruminebot.command.tool.User;
 import kz.ferius_057.ruminebot.command.tool.UserInPeerId;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @author Charles_Grozny
@@ -41,6 +43,7 @@ public class Resync extends AbstractCommand {
             return;
         }
 
+        ArrayList<String> users = new ArrayList<>();
         List<Integer> admins = new ArrayList<>();
 
         membersResponse.getItems().forEach(s -> {
@@ -48,13 +51,7 @@ public class Resync extends AbstractCommand {
         });
 
         membersResponse.getProfiles().forEach(s -> {
-            if (vkApi.getUsers().add(s.getId())) {
-               /* try {
-                    user(s.getId());
-                } catch (ClientException | ApiException e) {
-                    e.printStackTrace();
-                }*/
-            }
+            users.add(s.getId().toString());
             UserInPeerId userInPeerId = vkApi.getChatDao().getUserInPeerId(message.getPeerId() + "_" + s.getId());
             if (admins.contains(s.getId())) {
                 if (userInPeerId == null) vkApi.getChatDao().addUserInPeerId(message.getPeerId() + "_" + s.getId(), s.getFirstName(), "1");
@@ -62,6 +59,14 @@ public class Resync extends AbstractCommand {
             } else {
                 if (userInPeerId == null) vkApi.getChatDao().addUserInPeerId(message.getPeerId() + "_" + s.getId(), s.getFirstName(), "0");
                 else vkApi.getChatDao().updatePeerId(message.getPeerId() + "_" + s.getId(), s.getFirstName(), "0", 1);
+            }
+        });
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                User.registrationUserInTheBot(vkApi, users.toArray(new String[0]));
+            } catch (ClientException | ApiException e) {
+                e.printStackTrace();
             }
         });
 
